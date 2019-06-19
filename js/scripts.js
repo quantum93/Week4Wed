2996299085e2b361d32ef442ed40c8e242aec052
Cord Walsh and Tae Lee <cord@>
2019-06-19 16:30:48 -0700

    This is for multiple dices case

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ var UserAccount = [];
 UserAccount.push($("#nameOne").val())
 UserAccount.push($("#nameTwo").val())
 
+// This is for swapping player
 var turn = 0
-
 function swapUser () {
   if (turn === 0) {
     turn = 1;
@@ -14,23 +14,25 @@ function swapUser () {
   }
 }
 
+
 // Under here, this works for sinlge player but not for two players
 function Player (name) {
   this.userName = name;
   this.currentScore = 0
 }
 
-Player.prototype.dicePlay = function () {
-  var diceOne = Math.floor((Math.random()*6) + 1);
-  var diceTwo = Math.floor((Math.random()*6) + 1);
-  var score = diceOne + diceTwo
+Player.prototype.dicePlay = function (diceNumber) {
+  var score = 0;
+  for (var i = 1; i <= diceNumber; i++) {
+    score += Math.floor((Math.random()*6) + 1);
+  }
   // console.log(score);
-  if (score === 1) {
+  if (score === diceNumber) {
     this.currentScore = 0
     swapUser()
     // console.log(this.currentScore);
   }
-  if (score > 1 && this.currentScore < 100){
+  if (score > diceNumber && this.currentScore < 100){
     this.currentScore += score
     // console.log("here" + this.currentScore);
   }
@@ -49,15 +51,16 @@ $(document).ready(function() {
     event.preventDefault();
     var inputNameOne = $("#nameOne").val();
     var inputNameTwo = $("#nameTwo").val();
+    var diceNumber = parseInt($("#dice").val());
     // console.log(inputNameOne);
     // console.log(inputNameTwo);
     if (turn === 0) {
-      var scoreOne = playerOne.dicePlay();
+      var scoreOne = playerOne.dicePlay(diceNumber);
       $("#userOne").text(inputNameOne);
       $("#playOne").text(scoreOne);
     }
     if (turn === 1) {
-      var scoreTwo = playerTwo.dicePlay();
+      var scoreTwo = playerTwo.dicePlay(diceNumber);
       $("#userTwo").text(inputNameTwo);
       $("#playTwo").text(scoreTwo);
     }
