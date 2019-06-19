aecca6b64e3c1d8b711353ce86a113f412a156ba
Cord Walsh and Tae Lee <tae@>
2019-06-19 14:21:36 -0700

    This is first attempt to apply Object-oriented approach.

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,100 @@
 // ------------------------------- business logic ---------------------------------------------
-function PlayerOne (name) {
-  this.nameOne = nameOne;
-  this.initialScore = 0;
-  this.scoreOne = score;
+
+// var UserAccount = [userOne, userTwo]
+
+// UserAccount.prototype.changeUser() = function() {
+//
+// }
+
+// Under here, this works for sinlge player but not for two players
+function Player (name) {
+  this.userName = name;
+  this.currentScore = 0
 }
 
-PlayerOne.prototype.roll(score) {
+Player.prototype.dicePlay = function () {
   var score = Math.floor((Math.random()*6) + 1);
+  console.log(score);
   if (score === 1) {
-    this.scoreOne = this.initialScore
-  } else {
-    this.scoreOne += score
+    this.currentScore = 0
+    console.log(this.currentScore);
   }
+  if (score > 1 && this.currentScore < 20){
+    this.currentScore += score
+    console.log("here" + this.currentScore);
+  }
+  if (this.currentScore >= 20) {
+    alert("Wow, You win!")
+  }
+  return this.currentScore
 }
 
-PlayerOne.prototype.hold() {
+// ------------------------------- user interface logic ---------------------------------------
+$(document).ready(function() {
+  var inputNameOne = $("#nameOne").val();
+  var inputNameTwo = $("#nameTwo").val();
 
-}
+  var currentPlay = new Player(name)
+  $("#play").click(function (event) {
+    event.preventDefault();
 
-// function PlayerTwo (name) {
-//   this.nameTwo = nameTwo;
-//   this.initialScore = 0;
-//   this.scoreTwo = score;
+    var scoreOne = currentPlay.dicePlay();
+    var scoreTwo = currentPlay.dicePlay();
+
+    $("#userOne").text(inputNameOne);
+    $("#playOne").text(scoreOne);
+
+    $("#userTwo").text(inputNameTwo);
+    $("#playTwo").text(scoreTwo);
+  })
+  // $("#hold").click(function (event) {
+  //   event.preventDefault();
+  //   var yourAccout = UserAccount.changeUser();
+  //   $("#userName").text(inputName);
+  //   $("#currentPlay").text(yourScore);
+  // })
+})
+
+// var allPlayer = new PlayerAccount();
+//
+// function PlayerAccount () {
+//   this.accounts = []
 // }
 //
-// PlayerTwo.prototype.roll(score) {
+// PlayerAccount.prototype.updateAccount = function () {
+//   this.accounts.push()
+// }
+//
+// function PlayerOne (name) {
+//   this.nameOne = nameOne;
+//   this.initialScore = 0;
+//   this.scoreOne = score;
+// }
+//
+// PlayerOne.prototype.roll(score) {
 //   var score = Math.floor((Math.random()*6) + 1);
 //   if (score === 1) {
-//     this.scoreTwo = this.initialScore
+//     this.scoreOne = this.initialScore
+//     console.log(this.scoreOne);
 //   } else {
-//     this.scoreTwo += score
+//     this.scoreOne += score
+//     console.log(this.scoreOne);
 //   }
 // }
 //
-// PlayerTwo.prototype.hold() {
+// PlayerOne.prototype.hold() {
 //
 // }
 
-// ------------------------------- user interface logic ---------------------------------------
-$(document).ready(function () {
-  $("").submit(function (event) {
-    event.preventDefault();
-    var inputPlayerOne = $("").val();
-    var inputPlayerTwo = $("").val();
-
-  });
-});
+// $(document).ready(function () {
+//   $("").submit(function (event) {
+//     event.preventDefault();
+//     var inputPlayerOne = $("").val();
+//     var inputPlayerTwo = $("").val();
+//     var currentPlayer = new PlayerOne(inputPlayerOne);
+//     $(".clickable").click(function() {
+//       $("#").fadeToggle();
+//       $("#").fadeToggle();
+//     });
+//   });
+// });
